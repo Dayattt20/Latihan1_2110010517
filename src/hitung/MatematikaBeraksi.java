5ce40ab300b4ff5f351754b87f5e0a5f480207fb
Dayattt20 <dayathero20@gmail.com>
2023-06-19 22:01:14 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,5 +1,13 @@
 package hitung;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+       //membuat objek
+       Matematika Dayat = new Matematika(1,7);
+       
+       System.out.println("Hasil penjumlahan:"+Dayat.setPenjumlahan());
+       System.out.println("Hasil pengurangan:"+Dayat.setPengurangan());
+       System.out.println("Hasil perkalian:"+Dayat.setPerkalian());
+       System.out.println("Hasil pembagian:"+Dayat.setPembagian());
+    }        
 }
